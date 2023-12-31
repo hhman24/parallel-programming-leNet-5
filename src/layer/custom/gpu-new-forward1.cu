82e5b56aa375bb12ccb3c78fce79ff633a2237b6
hhman24 <anhoang483@gmail.com>
2023-12-31 11:38:02 +0700

    hhman - optimize constant

--- src/layer/custom/gpu-new-forward1.cu
+++ src/layer/custom/gpu-new-forward1.cu
@@ -3,53 +3,56 @@
 #include <cmath>
 #include <iostream>
 
-#define TILE_WIDTH 16
+#define MAX_OUTPUT_KERNEL 16
+#define MAX_INPUT_KERNEL 6
+#define KERNEL_WIDTH 5
 
-__global__ void conv_forward_kernel(float *output, const float *input, const float *kernel,
-                                    const int num_samples, const int output_channel, const int input_channel,
+__constant__ float dc_filter[MAX_OUTPUT_KERNEL * MAX_INPUT_KERNEL * KERNEL_WIDTH * KERNEL_WIDTH];
+
+__global__ void conv_forward_kernel(float *output, const float *input, const int num_samples,
+                                    const int output_channel, const int input_channel,
                                     const int height, const int width, const int kernel_size)
 {
     const int height_out = height - kernel_size + 1;
     const int width_out = width - kernel_size + 1;
 
     int height_grid = ceil(1.0 * height_out / TILE_WIDTH);
-    int width_grid = ceil(1.0 * width_out / TILE_WIDTH); 
-    
-    int batch_idx = blockIdx.x;        // batch number
-    int output_feature_idx = blockIdx.y; // output feature
+    int width_grid = ceil(1.0 * width_out / TILE_WIDTH);
+
+    int batch_idx = blockIdx.x;                                         // batch number
+    int output_feature_idx = blockIdx.y;                                // output feature
     int row_idx = (blockIdx.z / width_grid) * TILE_WIDTH + threadIdx.y; // row of the image matrix
     int col_idx = (blockIdx.z % width_grid) * TILE_WIDTH + threadIdx.x; // col of the image matrix
-    
+
     float accumulator = 0.0f;
 
-    if (row_idx < height_out && col_idx < width_out) 
+    if (row_idx < height_out && col_idx < width_out)
     {
-        for(int input_channel_idx = 0; input_channel_idx < input_channel; input_channel_idx++)             // sum over all input features
+        for (int input_channel_idx = 0; input_channel_idx < input_channel; input_channel_idx++) // sum over all input features
         {
-            for(int kernel_row = 0; kernel_row < kernel_size; kernel_row++)         // kernel_size x kernel_size filter 
+            for (int kernel_row = 0; kernel_row < kernel_size; kernel_row++) // kernel_size x kernel_size filter
             {
-                for(int kernel_col = 0; kernel_col < kernel_size; kernel_col++)
+                for (int kernel_col = 0; kernel_col < kernel_size; kernel_col++)
                 {
                     int input_row = row_idx + kernel_row;
                     int input_col = col_idx + kernel_col;
-                    accumulator += input[(batch_idx * (input_channel * height * width)) + 
-                                        (input_channel_idx * (height * width)) + 
-                                        (input_row * width) + 
-                                        input_col] *
-                                    kernel[(output_feature_idx * (input_channel * kernel_size * kernel_size)) + 
-                                            (input_channel_idx * (kernel_size * kernel_size)) + 
-                                            (kernel_row * kernel_size) + 
-                                            kernel_col];
+                    accumulator += input[(batch_idx * (input_channel * height * width)) +
+                                         (input_channel_idx * (height * width)) +
+                                         (input_row * width) +
+                                         input_col] *
+                                   dc_filter[(output_feature_idx * (input_channel * kernel_size * kernel_size)) +
+                                             (input_channel_idx * (kernel_size * kernel_size)) +
+                                             (kernel_row * kernel_size) +
+                                             kernel_col];
                 }
             }
         }
-        output[(batch_idx * (output_channel * height_out * width_out)) + 
-               (output_feature_idx * (height_out * width_out)) + 
-               (row_idx * width_out) + 
+        output[(batch_idx * (output_channel * height_out * width_out)) +
+               (output_feature_idx * (height_out * width_out)) +
+               (row_idx * width_out) +
                col_idx] = accumulator;
     } // endif (row_idx < height_out && col_idx < width_out)
 }
-
 
 __host__ void GPUInterface::conv_forward_gpu_full(float *output_data, const float *input_data, const float *weight_data,
                                                   const int num_samples, const int output_channel, const int input_channel,
@@ -60,13 +63,12 @@ __host__ void GPUInterface::conv_forward_gpu_full(float *output_data, const floa
 
     // Allocate device memory
     float *device_input, *device_output, *device_weight;
-    cudaMalloc((void **)&device_input, num_samples * input_channel * height_in * width_in * sizeof(float));  // input features map is input_channel
-    cudaMalloc((void **)&device_output, num_samples * output_channel * height_out * width_out * sizeof(float));  // output feature map is output_channel
-    cudaMalloc((void **)&device_weight, output_channel * input_channel * kernel_height * kernel_height * sizeof(float));  // input_channel * output_channel filter Maps of size kernel_height * kernel_height
+    cudaMalloc((void **)&device_input, num_samples * input_channel * height_in * width_in * sizeof(float));     // input features map is input_channel
+    cudaMalloc((void **)&device_output, num_samples * output_channel * height_out * width_out * sizeof(float)); // output feature map is output_channel
 
     // Copy input and mask data to device
     cudaMemcpy(device_input, input_data, num_samples * input_channel * height_in * width_in * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(device_weight, weight_data, output_channel * input_channel * kernel_height * kernel_height * sizeof(float), cudaMemcpyHostToDevice);
+    CHECK(cudaMemcpyToSymbol(dc_filter, weight_data, output_channel * input_channel * kernel_height * kernel_height * sizeof(float)));
 
     // Set the kernel dimensions and call the kernel
     int height_grid = ceil(1.0 * height_out / TILE_WIDTH);
@@ -76,7 +78,7 @@ __host__ void GPUInterface::conv_forward_gpu_full(float *output_data, const floa
     dim3 num_blocks_in_grid(num_samples, output_channel, Z);
 
     // Launch the kernel
-    conv_forward_kernel<<<num_blocks_in_grid, num_threads_per_block>>>(device_output, device_input, device_weight, num_samples, output_channel, input_channel, height_in, width_in, kernel_height);
+    conv_forward_kernel<<<num_blocks_in_grid, num_threads_per_block>>>(device_output, device_input, num_samples, output_channel, input_channel, height_in, width_in, kernel_height);
 
     // Copy the output back to host
     cudaMemcpy(output_data, device_output, num_samples * output_channel * height_out * width_out * sizeof(float), cudaMemcpyDeviceToHost);
@@ -84,5 +86,4 @@ __host__ void GPUInterface::conv_forward_gpu_full(float *output_data, const floa
     // Free device memory
     cudaFree(device_input);
     cudaFree(device_output);
-    cudaFree(device_weight);
 }
