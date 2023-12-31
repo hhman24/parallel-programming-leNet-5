1ef6b14bcea5ca52ffa8f35bda696c6e38556624
tienhoangggg <20127424@student.hcmus.edu.vn>
2023-12-31 16:14:45 +0700

    test shared memory

--- src/layer/custom/gpu-new-forward3.cu
+++ src/layer/custom/gpu-new-forward3.cu
@@ -5,49 +5,53 @@
 
 #define TILE_WIDTH 16
 
+//same as conv_forward_kernel in gpu-new-forward.cu but with shared memory optimization
 __global__ void conv_forward_kernel(float *output, const float *input, const float *kernel,
                                     const int num_samples, const int output_channel, const int input_channel,
                                     const int height, const int width, const int kernel_size)
 {
     const int height_out = height - kernel_size + 1;
     const int width_out = width - kernel_size + 1;
-
+    
     int height_grid = ceil(1.0 * height_out / TILE_WIDTH);
     int width_grid = ceil(1.0 * width_out / TILE_WIDTH); 
-    
+
     int batch_idx = blockIdx.x;        // batch number
     int output_feature_idx = blockIdx.y; // output feature
     int row_idx = (blockIdx.z / width_grid) * TILE_WIDTH + threadIdx.y; // row of the image matrix
     int col_idx = (blockIdx.z % width_grid) * TILE_WIDTH + threadIdx.x; // col of the image matrix
-    
-    float accumulator = 0.0f;
 
-    if (row_idx < height_out && col_idx < width_out) 
+    __shared__ float shared_input[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float shared_kernel[TILE_WIDTH][TILE_WIDTH];
+
+    if (row_idx < height && col_idx < width)
     {
-        for(int input_channel_idx = 0; input_channel_idx < input_channel; input_channel_idx++)             // sum over all input features
+        shared_input[threadIdx.y][threadIdx.x] = input[(batch_idx * (input_channel * height * width)) + 
+                                                        (threadIdx.y * (height * width)) + 
+                                                        (row_idx * width) + 
+                                                        col_idx];
+        shared_kernel[threadIdx.y][threadIdx.x] = kernel[(output_feature_idx * (input_channel * kernel_size * kernel_size)) + 
+                                                          (threadIdx.y * (kernel_size * kernel_size)) + 
+                                                          (row_idx * kernel_size) + 
+                                                          col_idx];
+    }
+    __syncthreads();
+
+    float accumulator = 0.0f;
+    if (row_idx < height_out && col_idx < width_out)
+    {
+        for(int i = 0; i < kernel_size; i++)
         {
-            for(int kernel_row = 0; kernel_row < kernel_size; kernel_row++)         // kernel_size x kernel_size filter 
+            for(int j = 0; j < kernel_size; j++)
             {
-                for(int kernel_col = 0; kernel_col < kernel_size; kernel_col++)
-                {
-                    int input_row = row_idx + kernel_row;
-                    int input_col = col_idx + kernel_col;
-                    accumulator += input[(batch_idx * (input_channel * height * width)) + 
-                                        (input_channel_idx * (height * width)) + 
-                                        (input_row * width) + 
-                                        input_col] *
-                                    kernel[(output_feature_idx * (input_channel * kernel_size * kernel_size)) + 
-                                            (input_channel_idx * (kernel_size * kernel_size)) + 
-                                            (kernel_row * kernel_size) + 
-                                            kernel_col];
-                }
+                accumulator += shared_input[threadIdx.y + i][threadIdx.x + j] * shared_kernel[i][j];
             }
         }
         output[(batch_idx * (output_channel * height_out * width_out)) + 
                (output_feature_idx * (height_out * width_out)) + 
                (row_idx * width_out) + 
                col_idx] = accumulator;
-    } // endif (row_idx < height_out && col_idx < width_out)
+    }
 }
 
 
